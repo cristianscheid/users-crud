3415dadc072e135f73d7c6e46c6e1e2c4d4f3966
Cristian <cristianscheid@gmail.com>
2024-08-15 21:41:53 -0300

    feat: install and configure react routes and change ident size to 2 spaces on .editorconfig

---
+++ react/src/router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./views/Login.jsx";
+import Signup from "./views/Signup.jsx";
+import Users from "./views/Users.jsx";
+import NotFound from "./views/NotFound.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/users",
+    element: <Users />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
