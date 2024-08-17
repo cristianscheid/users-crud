195867bfd97ac7573d285964810960f03ff902f1
Cristian <cristianscheid@gmail.com>
2024-08-17 08:33:36 -0300

    feat(front): create default and guest layouts, dashboard page and adjust routes

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,21 +1,44 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import DefaultLayout from "./components/DefaultLayout.jsx";
+import GuestLayout from "./components/GuestLayout.jsx";
+import Users from "./views/Users.jsx";
+import Dashboard from "./views/Dashboard.jsx";
 import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
-import Users from "./views/Users.jsx";
 import NotFound from "./views/NotFound.jsx";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <Login />,
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/users" />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+    ],
   },
   {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/users",
-    element: <Users />,
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
   },
   {
     path: "*",
