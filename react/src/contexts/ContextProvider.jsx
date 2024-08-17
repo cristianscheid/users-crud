3507c04fdb2eaa33cf004499484134d6fa40e1d9
Cristian <cristianscheid@gmail.com>
2024-08-17 10:27:43 -0300

    feat(front): create default layout

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -8,8 +8,11 @@ const StateContext = createContext({
 });
 
 export const ContextProvider = ({ children }) => {
-  const [user, setUser] = useState({});
-  const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+  const [user, setUser] = useState({
+    name: "John",
+  });
+  // const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+  const [token, _setToken] = useState(123);
 
   const setToken = (token) => {
     _setToken(token);
