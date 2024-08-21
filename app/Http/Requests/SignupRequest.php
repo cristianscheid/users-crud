def639fd5be05553506389bbbdf352af323ee336
Cristian <cristianscheid@gmail.com>
2024-08-21 07:39:23 -0300

    fix: adjust method name on axios client and wrong import on signup request file

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\Rules\Password;
 
 class SignupRequest extends FormRequest
 {
--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -5,7 +5,7 @@ const axiosClient = axios.create({
 });
 
 axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.get("ACCESS_TOKEN");
+  const token = localStorage.getItem("ACCESS_TOKEN");
   config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
