44dbe6536079a1c665111156ca83c8eddc0b8059
Cristian <cristianscheid@gmail.com>
2024-08-22 19:44:51 -0300

    feat(front): finish signup

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import axiosClient from "../axios-client";
 import { useStateContext } from "../contexts/ContextProvider.jsx";
@@ -8,7 +8,7 @@ export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmationRef = useRef();
-
+  const [errors, setErrors] = useState(null);
   const { setUser, setToken } = useStateContext();
 
   const onSubmit = (ev) => {
@@ -31,7 +31,7 @@ export default function Signup() {
       .catch((err) => {
         const response = err.response;
         if (response && response.status === 422) {
-          console.log(response.data.errors);
+          setErrors(response.data.errors);
         }
       });
   };
@@ -41,6 +41,13 @@ export default function Signup() {
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Signup for free</h1>
+          {errors && (
+            <div className="alert">
+              {Object.keys(errors).map((key) => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          )}
           <input ref={nameRef} type="text" placeholder="Full Name" />
           <input ref={emailRef} type="email" placeholder="Email Address" />
           <input ref={passwordRef} type="password" placeholder="Password" />
