2e56a65f6efcfa73abd7e29c842a8a4ce0dc3544
Cristian <cristianscheid@gmail.com>
2024-08-24 10:13:18 -0300

    feat: implement logout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,8 +1,10 @@
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useEffect } from "react";
+import axiosClient from "../axios-client";
 
 export default function DefaultLayout() {
-  const { user, token } = useStateContext();
+  const { user, token, setUser, setToken } = useStateContext();
 
   if (!token) {
     return <Navigate to="/login" />;
@@ -10,7 +12,17 @@ export default function DefaultLayout() {
 
   const onLogout = (ev) => {
     ev.preventDefault();
+    axiosClient.post("/logout").then(() => {
+      setUser({});
+      setToken(null);
+    });
   };
+
+  useEffect(() => {
+    axiosClient.get("/user").then(({ data }) => {
+      setUser(data);
+    });
+  }, []);
 
   return (
     <div id="defaultLayout">
@@ -23,7 +35,9 @@ export default function DefaultLayout() {
           <div>Header</div>
           <div>
             {user.name}
-            <a href="#" onClick={onLogout} className="btn-logout">Logout</a>
+            <a href="#" onClick={onLogout} className="btn-logout">
+              Logout
+            </a>
           </div>
         </header>
         <main>
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
